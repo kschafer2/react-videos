d1b9ed69cebd56f35e1c92a04f7146b07e9dc48b
Kyle Schafer <kschay@protonmail.com>
2019-08-22 19:07:12 -0500

    Closes #3 - Created VideoList Component

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,7 @@
 // npm install axios@0.18.1
 import axios from 'axios';
 
+//https://console.developers.google.com/apis
 const KEY = 'AIzaSyBKMQv89N_W9878uW6G40QlV8CKUwN7Zas';
 
 export default axios.create({
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
   state = { videos: [] };
 
-  onTermSubmit = async term => {
+  onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
         q: term
@@ -19,6 +20,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
